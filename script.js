90f56ceb46cf71bd8683ed3b15554a4c96364978
Yovel-R <yovel2911@gmail.com>
2024-12-12 21:52:33 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Show the full image in the modal
+
 function showFullImage(img) {
     const modal = document.getElementById('imageModal');
     const fullImage = document.getElementById('fullImage');
@@ -6,7 +6,7 @@ function showFullImage(img) {
     modal.style.display = 'flex'; // Show the modal
 }
 
-// Close the modal
+
 function closeModal() {
     const modal = document.getElementById('imageModal');
     modal.style.display = 'none'; // Hide the modal
